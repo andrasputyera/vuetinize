c258d0780db474d80482dc95b619b2bf1da62115
andrasputyera <andras.putyera@gmail.com>
2021-04-14 19:08:42 +0200

    set up adding & deleting tasks

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-pencil-plus-outline"
+      hide-details
+      clearable
+    ></v-text-field>
+    
     <v-list
       class="pt-0"
       flat
@@ -23,11 +35,21 @@
 
             <v-list-item-content>
               <v-list-item-title
-                :class="{ 'text-decoration-line-through' : task.done }""
+                :class="{ 'text-decoration-line-through' : task.done }"
               >
                 {{ task.title }}
               </v-list-item-title>
             </v-list-item-content>
+
+            <v-list-item-action>
+              <!-- The stop method is used to prevent triggering parent element -->
+              <v-btn 
+              @click.stop="deleteTask(task.id)"
+              icon
+              >
+                <v-icon color="teal lighten-3">mdi-delete-empty-outline</v-icon>
+              </v-btn>
+            </v-list-item-action> 
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -44,6 +66,7 @@ export default {
   name: 'Todo',
   data() {
     return {
+      newTaskTitle: '',
       tasks: [
         {
           id: 1,
@@ -64,9 +87,21 @@ export default {
     }
   },
   methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    },
     doneTask(id) {
       let task = this.tasks.filter(task => task.id === id)[0]
       task.done = !task.done
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
     }
   }
 }
