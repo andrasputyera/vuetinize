b541fb1491cd86e041dde1c0b3711eb022c3e54e
andrasputyera <andras.putyera@gmail.com>
2021-04-14 13:56:42 +0200

    created list of tasks, added data property & marked tasks as done

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,39 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo Page</h1>
+  <div class="home">
+    <v-list
+      class="pt-0"
+      flat
+    >
+      <div
+        v-for="task in tasks" 
+        :key="task.id"
+      >
+        <v-list-item
+          @click="doneTask(task.id)" 
+          :class="{ 'teal lighten-5' : task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <!-- Input value determines whether task was done (checks the checkbox)-->
+              <v-checkbox
+                :input-value="task.done"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through' : task.done }""
+              >
+                {{ task.title }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+      
+    </v-list>
   </div>
 </template>
 
@@ -9,7 +42,32 @@
 
 export default {
   name: 'Todo',
-  components: {
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Task 1',
+          done: false
+        },
+        {
+          id: 2,
+          title: 'Task 2',
+          done: false
+        },
+        {
+          id: 3,
+          title: 'Task 3',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    }
   }
 }
 </script>
